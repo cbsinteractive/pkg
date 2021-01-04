0951c16e08d6a4a7683951879b107e280aad75a2
Thomas Symborski <thomas.symborski@gmail.com>
2021-01-04 10:50:54 -0500

    framerate: adds Empty helper func

--- video/framerate.go
+++ video/framerate.go
@@ -6,3 +6,7 @@ type Framerate struct {
 	Numerator   int `json:"numerator,omitempty" redis-hash:"numerator,omitempty"`
 	Denominator int `json:"denominator,omitempty" redis-hash:"denominator,omitempty"`
 }
+
+func (f Framerate) Empty() bool {
+	return f.Denominator == 0 || f.Numerator == 0
+}
